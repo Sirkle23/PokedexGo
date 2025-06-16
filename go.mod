8c4f026c7e3c1f4dabd9021bfefe95f84a0a3860
Stefan <s.t.kempers@tue.nl>
2025-06-16 10:01:40 +0200

    Created REPL

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Sirkle23/PokedexGo
+
+go 1.24.3
--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"bufio"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Pokedex > ")
+	for scanner.Scan() {
+		line := scanner.Text()
+		
+		cleanedWords := cleanInput(line)
+		fmt.Printf("Your command was: %s\n", cleanedWords[0])
+		
+		fmt.Print("Pokedex > ")
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Errorf("error while scanning bufio: %v", err)
+	}
+}
+
+func cleanInput(text string) []string {
+	// Split the input text into words
+	words := []string{}
+	for _, word := range strings.Split(text, " ") {
+		// Convert each word to lowercase and trim spaces
+		cleanedWord := strings.Trim(word, ",.!?")
+		cleanedWord = strings.ToLower(cleanedWord)
+		if cleanedWord != "" {
+			words = append(words, cleanedWord)
+		}
+	}
+
+	// Return the cleaned words
+	return words
 }
